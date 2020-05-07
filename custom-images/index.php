3e4f5701ee7b4dc1412b69830d5fe92d838fae56
mika <1506612+elmika@users.noreply.github.com>
2020-05-07 23:13:02 +0200

    Show memory usage in script

--- custom-images/index.php
+++ custom-images/index.php
@@ -4,4 +4,9 @@ echo "Hello world of Docker! Este es un autodeploy a Docker hub!";
 var_dump($message);
 var_dump($inexistent_message);
 
+
+exec("free -mtl", $output);
+print_r($output);
+
+
 // syntax error
